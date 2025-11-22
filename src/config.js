7e3ffb6cb14b13115b1387f09a8bbdae4de89871
dominkve <dominikbogmic8@gmail.com>
2025-11-22 17:50:56 +0100

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var config = {
+    apiKey: "AIzaSyAfjGkYuKVHmES-plwEQANUu1wRYxHFsBw",
+    authDomain: "aplikacija-21276.firebaseapp.com",
+    projectId: "aplikacija-21276",
+    storageBucket: "aplikacija-21276.firebasestorage.app",
+    messagingSenderId: "268238860819",
+    appId: "1:268238860819:web:da620228aa41aa64bf05e6",
+    measurementId: "G-HMTTWPWD75"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+// Google OAuth Client ID, needed to support One-tap sign-up.
+// Set to null if One-tap sign-up is not supported.
+var CLIENT_ID =
+    null; // 'YOUR_OAUTH_CLIENT_ID';
