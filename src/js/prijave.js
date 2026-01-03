216a8738a31ed7c606023b76b909a4a9636e7c13
dominkve <dominikbogmic8@gmail.com>
2026-01-03 21:37:00 +0100

    fetch data

---
+++ src/js/prijave.js
@@ -0,0 +1,22 @@
+async function getData() {
+    const url = "../../public/broj_prijava.json";
+
+    return await fetch(url)
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .catch((err) => {
+            console.log("Error:", err);
+            return null;;
+        })
+};
+
+async function main() {
+    console.log("App starting...");
+    const json = await getData();
+
+    
+};
+
+main();
